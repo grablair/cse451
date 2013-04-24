9d77af45776be13da30da725afb689ac2c7c13fd
Graham Arthur Blair <graham@grahamarthurblair.com>
2013-04-24 14:37:13 -0700

    fixed utility

--- project1/utility/execcnts.c
+++ project1/utility/execcnts.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "../counts/getexeccounts.h"
+#include "getexeccounts.h"
 
 void usage() {
-	printf("Usage: execcnts <program>\n\tRuns the given program and shows it's exec/fork/clone counts.\n");
+	printf("Usage: execcnts <program>\n");
+	printf("       Runs the given program and shows it's exec/fork/clone counts.\n");
 	exit(EXIT_FAILURE);
 }
 
 int main(int argc, char **argv) {
 	if (argc < 2) usage();
 
+	int pArray[] = {0, 0, 0, 0};
+	getExecCounts(getpid(), pArray);
+
 	int child_pid;
 	if (child_pid = fork()) {
 		wait(child_pid);
-		int pArray[] = {0, 0, 0, 0};
-		getExecCounts(child_pid, pArray);
-		printf("\n%d\tforks\n", pArray[0]);
-		printf("%d\tvforks\n", pArray[1]);
-		printf("%d\texecves\n", pArray[2]);
-		printf("%d\tclones\n", pArray[3]);
+		int pArray2[] = {-1, 0, 0, 0};	// Set to -1 because of utility's fork
+		getExecCounts(getpid(), pArray2);
+		printf("\n%d\tforks\n", pArray2[0] - pArray[0]);
+		printf("%d\tvforks\n", pArray2[1] - pArray[1]);
+		printf("%d\texecves\n", pArray2[2] - pArray[2]);
+		printf("%d\tclones\n", pArray2[3] - pArray[3]);
 	} else {
 		// Launch new program
 		char *file = argv[1];
